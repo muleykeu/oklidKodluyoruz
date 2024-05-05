08b8508126d0ef9e651c19fe5c9b628576edbdce
Müleyke Uysal <muleyke8@gmail.com>
2024-05-05 23:57:23 +0300

    kodluyoruz python uygulaması
    kodluyoruz python uygulaması

---
+++ oklid/oklid.py
@@ -0,0 +1,18 @@
+import math
+
+points = [(5, 12), (3, 4), (7, 24), (6, 8)]
+
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+distances = []
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_mesafe = min(distances)
+
+print("Minimum mesafe:", min_mesafe)
